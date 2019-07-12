dc98c354fab7d2a58227e6311c301589f4dc146d
yoovin <soo22839@gmail.com>
2019-07-13 08:22:04 +0900

    19/07/13

--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -1,27 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types'
 import './Movie.css';
 
-class Movie extends Component{
-
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
-    }
-
-    render(){
-        return(
-        <div>
-        <MoviePoster poster={this.props.poster}/>
-        <h1>{this.props.title}</h1>
-        </div>
-        
-        );
-    }
-}
 
 function Movie({title, poster}){
-    
+    return(
+        <div>
+        <MoviePoster poster={poster}/>
+        <h1>{title}</h1>
+        </div>
+    )
+}
+Movie.propTypes = {
+    title:PropTypes.string.isRequired,
+    poster:PropTypes.string.isRequired
 }
 
 function MoviePoster({poster}){
