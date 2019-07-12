2fcbfc41a008cb655418b2d1ed19524cc3041ef3
Yoovin <soo22839@gmail.com>
2019-07-13 07:06:45 +0900

    19/07/13

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -2,20 +2,31 @@ import React from 'react';
 import './App.css';
 import Movie from './Movie'
 
-const ani = [
-  "카구야 님은 고백받고 싶어",
-  "5등분의 신부",
-  "사신 도련님과 검은 메이드",
-  "바보와 시험과 소환수"
+const anis = [
+  {
+    title: "카구야 님은 고백받고 싶어",
+    poster: "http://img.chuing.net/i/QNpVGVy/4567.png"
+  },
+  {
+    title: "5등분의 신부",
+    poster: "https://shop.r10s.jp/amiami/cabinet/images/2019/12/card-00008461.jpg"
+  },
+  {
+    title: "사신 도련님과 검은 메이드",
+    poster: "https://img.ironmancdn.xyz/upload/be36353103ec7a2b7f385696cbe60d98.JPEG?v=1?quick"
+  },
+  {
+    title: "팝팀에픽",
+    poster: "https://i.ytimg.com/vi/nPO_ZzoELKo/maxresdefault.jpg"
+  }
 ]
 
 function App() {
   return (
     <div className="App">
-     <Movie title={ani[0]}/>
-     <Movie title={ani[1]}/>
-     <Movie title={ani[2]}/>
-     <Movie title={ani[3]}/>
+      {anis.map((ani, index) => {
+        return <Movie title={ani.title} poster={ani.poster} key={index} />
+      })}
     </div>
   );
 }
--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 import './Movie.css';
 
 class Movie extends Component{
+
+    static propTypes = {
+        title: PropTypes.string,
+        poster: PropTypes.string
+    }
+
     render(){
         return(
+        <div>
+        <MoviePoster poster={this.props.poster}/>
         <h1>{this.props.title}</h1>
+        </div>
+        
+        );
+    }
+}
+
+class MoviePoster extends Component{
+    render(){
+        return(
+            <img src={this.props.poster} />
         );
     }
 }
